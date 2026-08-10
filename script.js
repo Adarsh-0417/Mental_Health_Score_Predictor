94550b5b7de64d559115e97a123ed23cedd0f28e
adarsh-0417 <adarshsharma0417@gmail.com>
2026-08-10 18:09:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:2200";
+const API_BASE_URL = "https://mental-health-score-predictor-1-syqz.onrender.com";
 const PREDICT_ENDPOINT = `${API_BASE_URL}/predict`;
 
 const form = document.getElementById("assessmentForm");
